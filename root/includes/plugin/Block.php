2a40a1e104418f0b62341af99cc4d252eb988a43
Luca Tumedei <luca@theaveragedev.com>
2014-03-13 09:48:52 +0100

    modified the Block.php file to reflect the static nature of actual Headway call implementation

--- root/includes/plugin/Block.php
+++ root/includes/plugin/Block.php
@@ -1,48 +1,51 @@
 <?php
 namespace {%= prefix %};
 
-        class Block extends \HeadwayBlockAPI {
+class Block extends \HeadwayBlockAPI {
 
-            public $id = '{%= prefix %}';
-            public $name = '{%= title %}';
-            public $options_class = '\{%= prefix %}\BlockOptions';
-            public $description = '{%= description %}';
+    public $id = '{%= prefix %}';
+    public $name = '{%= title %}';
+    public $options_class = '\{%= prefix %}\BlockOptions';
+    public $description = '{%= description %}';
 
-            function enqueue_action($block_id) {
-                /* CODE FOR OPTIONAL METHOD */
-                return;
-            }
+    // public static function init_action($block_id, $block) 
+    // {
 
-            function init_action($block_id) {
-                /* CODE FOR OPTIONAL METHOD */
-                return;
-            }
+    // }
 
-            function dynamic_js($block_id) {
-                /* CODE FOR OPTIONAL METHOD */
-                return;
-            }
 
-            function dynamic_css($block_id) {               
-                /* CODE FOR OPTIONAL METHOD */
-                return;
-            }
+    // public static function enqueue_action($block_id, $block, $original_block = null)
+    // {
 
-            function setup_elements() {
-                $this->register_block_element(array(
-                    'id' => 'element1-id',
-                    'name' => 'Element 1 Name',
-                    'selector' => '.my-selector1',
-                    'properties' => array('property1', 'property2', 'property3'),
-                    'states' => array(
-                        'Selected' => '.my-selector1.selected',
-                        'Hover' => '.my-selector1:hover',
-                        'Clicked' => '.my-selector1:active'
-                    )
-                ));
-            }
+    // }
 
-            function content($block) {
-                /* CODE HERE */
-            }
-        }
+
+    // public static function dynamic_css($block_id, $block, $original_block = null)
+    // {
+
+    // }
+
+
+    // public static function dynamic_js($block_id, $block, $original_block = null)
+    // {
+
+    // }
+
+    // public function setup_elements() {
+    //     $this->register_block_element(array(
+    //         'id' => 'element1-id',
+    //         'name' => 'Element 1 Name',
+    //         'selector' => '.my-selector1',
+    //         'properties' => array('property1', 'property2', 'property3'),
+    //         'states' => array(
+    //             'Selected' => '.my-selector1.selected',
+    //             'Hover' => '.my-selector1:hover',
+    //             'Clicked' => '.my-selector1:active'
+    //             )
+    //         ));
+    // }
+
+    public function content($block) {
+        /* CODE HERE */
+    }
+}
