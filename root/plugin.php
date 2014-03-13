b73b20801f9ca976ba299c20e1fc70f8aab895bc
Luca Tumedei <luca@theaveragedev.com>
2014-03-13 09:13:51 +0100

    modified the plugin.php file to autoload all PSR-0 names in the includes folder

--- root/plugin.php
+++ root/plugin.php
@@ -19,8 +19,7 @@
  * your discretion, any later version, as published by the Free
  * Software Foundation.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * This program is distributed in the hope that it will be useful, * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
@@ -30,15 +29,18 @@
  */
 
 // Useful global constants
-define( '{%= prefix_caps %}_BLOCK_VERSION', '0.1.0' );
-define( '{%= prefix_caps %}_BLOCK_URL',     plugin_dir_url( __FILE__ ) );
-define( '{%= prefix_caps %}_BLOCK_PATH',    dirname( __FILE__ ) . '/' );
+define( '{%= prefix_caps %}_BLOCK_VERSION', '0.1.0');
+define( '{%= prefix_caps %}_BLOCK_URL', plugin_dir_url( __FILE__));
+define( '{%= prefix_caps %}_BLOCK_PATH', dirname( __FILE__) . '/');
 // Include the class loader if it's not defined already
 if (!class_exists('jwage\SplClassLoader')) {
     require_once dirname(__FILE__) . '/includes/jwage/SplClassLoader.php';
 }
-// Add the plugin namespace to the autolaoding classes
-$classLoader = new \jwage\SplClassLoader('{%= prefix %}', dirname(__FILE__) . '/includes');
-$classLoader->register();
+// Add all the folders in the includes folder to autoloading
+foreach (glob(dirname(__FILE__) . '/includes/*', GLOB_ONLYDIR) as $folderPath) {
+    $folderName = basename($folderPath);
+    $classLoader = new \jwage\SplClassLoader($folderName, dirname(__FILE__) . '/includes');
+    $classLoader->register();
+}
 // boostrap the block
 new \{%= prefix %}\Main();
